fe6eae29ebefcc8746618a9f62dc1fe534b3d81d
seucra <tabrezahmed.sta33@gmail.com>
2025-04-19 02:52:41 +0530

    new file:   BTech/Year 1/Java/J106thisKeywordSpecialUse.java 	new file:   BTech/Year 1/Java/J107JRuntimeDataIp.class 	new file:   BTech/Year 1/Java/J107JRuntimeDataIp.java 	new file:   BTech/Year 1/Java/J107JRuntimeDataIp2.java 	deleted:    BTech/Year 1/Java/Applet/AppletHelloWorld.class 	deleted:    BTech/Year 1/Java/Applet/Background.class 	deleted:    BTech/Year 1/Java/Applet/EmbeddedHtml.class 	deleted:    BTech/Year 1/Java/Applet/PassingValues.class 	deleted:    BTech/Year 1/Java/J107JRuntimeDataIp.class

---
+++ BTech/Year 1/Java/J107JRuntimeDataIp.java
@@ -0,0 +1,10 @@
+//Command line input in java
+
+public class J107JRuntimeDataIp {
+    public static void main(String[] args) {
+        for (int i=0; i<args.length; i++){
+            System.out.print(args[i]+" \n");
+        }
+        System.exit(0);
+    }
+}
---
+++ BTech/Year 1/Java/J107JRuntimeDataIp2.java
@@ -0,0 +1,7 @@
+//Command line input in java
+
+public class J107JRuntimeDataIp2 {
+    public static void main(String[] args) {
+        System.out.print("Hi "+args[0]+", How are you?");
+    }
+}
