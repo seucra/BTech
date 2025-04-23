f538ed378bd039c2c429016694d90a98f96057df
seucra <tabrezahmed.sta33@gmail.com>
2025-04-23 13:46:27 +0530

    modified:   Projects/Maths/ContactManagementSystem.c 	deleted:    matrix 	deleted:    matrix.txt

--- Projects/Maths/ContactManagementSystem.c
+++ Projects/Maths/ContactManagementSystem.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+// Struct DEfinitions
 typedef struct Contact
 {
     char name[50], phone[15], email[50];
@@ -6,24 +9,48 @@ typedef struct Contact
 typedef struct Node
 {
     Contact contact;
-    struct Node *next, *left, *right;
+    struct Node *next;  // For LL
+    struct Node *left, *right; // For BST
 }Node;
 
-struct GraphNode
+typedef struct GraphNode
 {
-    int contactID;
+    int contactID; // Index in Contacts[] array
     struct GraphNode *next;
 }GraphNode;
 
-struct QueueNode
+typedef struct QueueNode
 {
-    char request[100];
+    char request[100];          // Eg: "add:John,1234567890,abs@cde.com"
     struct QueueNode *next;
 }QueueNode;
+
+typedef struct Queue
+{
+    QueueNode *front, *rear;
+}Queue;
+
+typedef struct StackNode
+{
+    char action[100];       // Eg: "add:hohn,1234567890"
+    struct StackNode *next;
+}StackNode;
+
+typedef struct Stack
+{
+    StackNode *top;
+}Stack;
+
+// Global Variables
+Contact contacts[100];      // Array for Contacts
+int contactcount = 0;
+Node *head = NULL;
 
 // Adding Contact
 void addContact();
 
+// Display 
+void Display();
 
 
 int main()
@@ -34,8 +61,8 @@ int main()
         int ch;
         if (scanf("%d", &ch) != 1)
         {
-            printf("Invalid Input. Please enter a valid number.\n");
-            while (getchar() != '\n')
+            printf("Invalid Input.\n");
+            while (getchar() != '\n');
             continue;
         }
         if (ch == 8)
@@ -52,31 +79,44 @@ int main()
                 printf("Enter email : ");
 
                 break;
+
             case 2:
                 printf("To Display Contact...");
 
                 break;
+
             case 3:
                 printf("Searching Contact...");
             
                 break;
+
             case 4:
                 printf("Syncing Contacts...");
             
                 break;
+            
             case 5:
                 printf("Undoing Last Action...");
+            
             
             case 6:
                 printf("Saving Contacts...");
                 
                 break;
+        
             case 7:
                 printf("Loading Contacts...");
             
                 break;
+    
             default:
                 printf("Invalid Input.\n");
+                if (scanf("%d", &ch) != 1)
+                {
+                    while (getchar() != '\n');
+                    continue;
+                }
+                break;
         }
     }
 }
